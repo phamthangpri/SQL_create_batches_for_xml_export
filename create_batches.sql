ac0059d30a1c65cb3ec45b1b0e01370b85eaf148
phamthangpri <phamthangpri@gmail.com>
2023-01-13 14:05:08 +0200

    add param and sp

--- create_batches.sql
+++ create_batches.sql
@@ -28,8 +28,20 @@ DECLARE
 		;
 SET @filename = 'prefix_file_name' + CAST(@year AS VARCHAR(4)) + '.' + @quarter
 				+ '.' CONVERT(VARCHAR,@generation_date,20)
--------------------------------------XML Export --------------------------------------------------------
----Create different batches to export -------------
+
+
+---- STEP 1: Create an id for each record 
+IF OBJECT_ID('tempdb..##TABLE_1') IS NOT NULL --vue par associate
+        DROP TABLE ##TABLE_1
+	SELECT  
+		FLOOR(((ROW_NUMBER() OVER (ORDER BY c.contract_id))-1)/10000)+1								AS batch_number,
+		c.contract_id 
+	INTO ##TABLE_1
+	FROM table_contract c
+END
+
+
+--- STEP 2: Create different batches to export -------------
 
 IF OBJECT_ID('tempdb..##BATCH_NUMBER') IS NOT NULL 
 		DROP TABLE ##BATCH_NUMBER
@@ -43,14 +55,30 @@ FROM ##TABLE_1
 GROUP BY batch_number
 ORDER BY batch_number
 
-
+-------------------------------------XML Export --------------------------------------------------------
 SELECT batch_number,
 		filename,
-	(SELECT 
-		(------children 1
+		(
 		SELECT
 			 @code_version																					AS 'xml/version'
 			,@generation_date																				AS 'xml/generation_date'
 			,LOWER(NEWID())																					AS 'xml/id'
+			,@year																							AS 'general/parameters/year'
+
+			FOR XML PATH(''),TYPE)																			AS associate																					
+			----- end associate
+		FROM ##TABLE_1 a
+		WHERE 
+			(@sample_auto = 1 AND EXISTS (SELECT 1 FROM ##SAMPLE_TEST st WHERE st.associateId = a.associateId))
+			OR @sample_auto = 0 
+			AND a.batch_number = b.batch_number
+		FOR XML PATH('associates'),TYPE
+		)																									AS [nealan/data]
+	FOR XML PATH(''),TYPE, ROOT('root')
+	)																					AS xml_file
+	FROM ##BATCH_NUMBER b
+OPTION(RECOMPILE)
+END
+
 
 FLOOR(((ROW_NUMBER() OVER (ORDER BY c.associateId))-1)/10000)+1								AS batch_number,
