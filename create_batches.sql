b89a2d1856650652f0fed10e6473ebdd6f6ba0f5
phamthangpri <phamthangpri@gmail.com>
2023-02-10 10:05:08 +0200

    update code

--- create_batches.sql
+++ create_batches.sql
@@ -2,16 +2,16 @@
  Author:            Thi Thang Pham
  Description:       Generat XML files by batch
 					
- Parameter(s):      @quarter
-					@generation_date 
-					@year
+ Parameter(s):      @quarter : string
+					@generation_date : date
+					@year : int
 
 Explaination:
 1. I'm gonna split the table_contract to batches of 10 000 rows
 2. Then I'll create a temporary table ##BATCH_NUMBER with the file name and the number of the batch (for ex: filename_batch_1_8.xml ~ that means the file 1 on 8 total)
 3. In the code to export XML, I need to join this temporary table to the data to export.
 
-The parameter is just in case you need to use the generation date
+The parameters are just in case you need to use the generation date into the filename
  *************************************************************************************************************/
 
  
